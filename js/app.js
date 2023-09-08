bf83adcf082916c9291fd8fc94550b973c590745
Ashaduzzaman10 <infoaj549814@gmail.com>
2023-09-08 18:47:08 +0600

    pin-generate

--- js/app.js
+++ js/app.js
@@ -17,3 +17,24 @@ document.getElementById("generate-pin").addEventListener("click", function () {
   const displayPin = document.getElementById("display-pin");
   displayPin.value = pin;
 });
+document
+  .getElementById("calculator")
+  .addEventListener("click", function (event) {
+    const number = event.target.innerText;
+    const typedNumber = document.getElementById("typed-number");
+    const previousTypedNumber = typedNumber.value;
+    if (isNaN(number)) {
+      if (number === "C") {
+        typedNumber.value = "";
+      } else if (number === "<") {
+        const digits = previousTypedNumber.split("");
+        digits.pop();
+        const remainingDigits = digits.join("");
+        typedNumber.value = remainingDigits;
+      }
+    } else {
+      const previousTypedNumber = typedNumber.value;
+      const newTypedNumber = previousTypedNumber + number;
+      typedNumber.value = newTypedNumber;
+    }
+  });
